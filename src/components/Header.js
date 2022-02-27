8af39f63de4f2d1f5082a00b5d017ec94e101b9f
Marto2356 <92894644+Marto2356@users.noreply.github.com>
2022-02-27 10:35:27 -0300

    Desafío de la Clase N°3 - JSX & transpiling

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import NavBar from './NavBar'
+
+function Header () {
+    return  <header>
+                <h1>CoderShop</h1>
+                <NavBar/>
+            </header>
+}
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+        </nav>
+    )
+}
+
+export default NavBar
